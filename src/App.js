8500f658c8afc94015fd6916cc8ad37c3f865253
Asif2535 <aiqbal517@gmail.com>
2021-05-28 17:11:33 +0600

    added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
@@ -7,6 +15,37 @@ function App() {
       <h1>First branch created</h1>
       <p>Added Redux branch</p>
       <p>Added api branch</p>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+              <li>
+                <Link to="/">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/">
+              <About />
+            </Route>
+            <Route path="/">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }

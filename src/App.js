6c92f1c04870bc1be3c7f9519f061946daf4e3c2
Asif2535 <aiqbal517@gmail.com>
2021-05-28 18:59:15 +0600

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NaviGationBar from './Components/NaviGationBar/NaviGationBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>First branch created</h1>
+      <NaviGationBar></NaviGationBar>
     </div>
   );
 }
---
+++ src/Components/NaviGationBar/NaviGationBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Form, FormControl, Nav, Navbar, NavDropdown, Button } from 'react-bootstrap';
+
+const NaviGationBar = () => {
+    return (
+        <div>
+            <Navbar bg="dark" expand="lg" variant="dark">
+                <Navbar.Brand className="text-danger" href="#">FilmFlick</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="mr-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Link</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                            Link
+                        </Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-danger">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
+};
+
+export default NaviGationBar;

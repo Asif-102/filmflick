398c22c8f554767e5a634d3c482706e8678e5178
Asif2535 <aiqbal517@gmail.com>
2021-05-29 00:36:21 +0600

    movi it to redux branch

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NaviGationBar from '../NaviGationBar/NaviGationBar';
+import MoviesList from './MoviesList/MoviesList';
 
 const Home = () => {
     return (
         <div>
             <NaviGationBar></NaviGationBar>
+            <MoviesList></MoviesList>
         </div>
     );
 };
--- src/Components/Home/MoviesList/MoviesList.js
+++ src/Components/Home/MoviesList/MoviesList.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { POPULAR_MOVIE_URL } from '../../API/API';
 
 const MoviesList = () => {
+    const [popularMovies, setPopularMovies] = useState([]);
+    useEffect(()=>{
+        fetch(POPULAR_MOVIE_URL)
+        .then(res => res.json())
+        .then(data => setPopularMovies(data.results))
+    },[])
     return (
-        <div>
-            
+        <div  className="container">
+            <h1>This is movie  List section</h1>
         </div>
     );
 };

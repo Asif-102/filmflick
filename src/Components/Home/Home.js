f44a454e28e35d855c8fbabc8005d357e53d14e5
shahnewaz601 <shahnewaz601@gmail.com>
2021-05-29 02:32:24 +0600

    top movies banner done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NaviGationBar from '../NaviGationBar/NaviGationBar';
+import TopMoviesBanner from '../TopMoviesBanner/TopMoviesBanner';
 
 const Home = () => {
     return (
         <div>
             <NaviGationBar></NaviGationBar>
+            <TopMoviesBanner></TopMoviesBanner>
         </div>
     );
 };
--- src/Components/TopMoviesBanner/TopMoviesBanner.js
+++ src/Components/TopMoviesBanner/TopMoviesBanner.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import './TopMoviesBanner.css';
 
 const TopMoviesBanner = () => {
+
     return (
-        <div>
-            
-        </div>
+        <section className="container">
+            <div className="topMovies">
+                <div className="vertical-movies">
+                    <ul className="list-inline d-flex  flex-column ml-4">
+                        <h3 className="mb-4 pt-4 text-white">Top 10 in Hollywood</h3>
+                        <li className="mt-3 topMovieItem">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Jurassic World</h5>
+                                    <div className="d-flex align-items-center pb-2">
+                                        <div className="badge badge-secondary p-1">Action</div>
+                                        <span className="ml-2">1hr : 28mins</span>
+                                    </div>
+                                    <div className="card-btn">
+                                        <a href="/" className="btn btn-style px-4">Play Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mt-3 topMovieItem">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Jurassic World</h5>
+                                    <div className="d-flex align-items-center pb-2">
+                                        <div className="badge badge-secondary p-1">Action</div>
+                                        <span className="ml-2">1hr : 28mins</span>
+                                    </div>
+                                    <div className="card-btn">
+                                        <a href="/" className="btn btn-style px-4">Play Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="mt-3 topMovieItem">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Jurassic World</h5>
+                                    <div className="d-flex align-items-center pb-2">
+                                        <div className="badge badge-secondary p-1">Action</div>
+                                        <span className="ml-2">1hr : 28mins</span>
+                                    </div>
+                                    <div className="card-btn">
+                                        <a href="/" className="btn btn-style px-4">Play Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 };
 

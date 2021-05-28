1be94e427cbf9c79219bf065e72531cb30a9fd3d
Asif2535 <aiqbal517@gmail.com>
2021-05-29 02:27:02 +0600

    show movies poster

--- src/Components/Home/MoviesList/MoviesList.js
+++ src/Components/Home/MoviesList/MoviesList.js
@@ -1,19 +1,26 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadMovies } from '../../../redux/actions/filmFlickActions';
+import MoviesCardLists from './MoviesCardLists/MoviesCardLists';
 
 const MoviesList = () => {
-    const movies = useSelector((state) =>{
+    const movies = useSelector((state) => {
         return state.movies.movieLists;
     })
 
     const dispatch = useDispatch();
-    
-    useEffect(()=> dispatch(loadMovies()) ,[])
+
+    useEffect(() => dispatch(loadMovies()), [])
 
     return (
-        <div  className="container">
-            <h1>Total Movies {movies.length}</h1>
+        <div className="container text-white">
+            <h3>Movies</h3>
+            <Row>
+                {
+                    movies?.map((allMovies)=>(<MoviesCardLists key={allMovies.id} allMovies={allMovies} />))
+                }
+            </Row>
         </div>
     );
 };

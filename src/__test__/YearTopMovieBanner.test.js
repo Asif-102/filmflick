66743ad7aa58e18410af11b579ad3c646ef8a068
shahnewaz601 <shahnewaz601@gmail.com>
2021-05-30 21:28:31 +0600

    update topMovieBanner unit testing

--- src/__test__/YearTopMovieBanner.test.js
+++ src/__test__/YearTopMovieBanner.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from  'react-dom';
 import YearTopMovieBanner from '../Components/Home/YearTopMovieBanner/YearTopMovieBanner';
 
+
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<YearTopMovieBanner></YearTopMovieBanner>, div)
